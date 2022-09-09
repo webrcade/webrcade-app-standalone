ac42caefe5dafff76dd0f0a09d8f98746a489bbc
raz0red <raz0red@gmail.com>
2022-09-08 23:11:00 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+import { dropbox, settings, LOG } from '@webrcade/app-common'
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      ready: false
+    };
+  }
+
+
+  componentDidMount() {
+    window._dropbox = dropbox;
+
+    settings.load().finally(() => {
+      dropbox.checkLinkResult()
+        .catch(e => { LOG.error(e) })
+        .finally(() => {
+          this.setState({ready: true});
+        })
+    })
+  }
+
+  render() {
+    const { ready } = this.state;
+
+    if (ready) {
+      return (
+        <iframe
+          width="100%"
+          height="100%"
+          frameBorder="0"
+          allow="autoplay; gamepad"
+          title={window._title}
+          src={window._appUrl} />
+      );
+    } else {
+      return (<></>);
+    }
+  }
+}
+
+export default App;
